7b5f78e2dc66890fa499196e69ea0ad1d1b14c65
Ben Kahle <ben@kahle.org>
2014-09-28 01:15:08 -0400

    Made Servos do things

--- sensingtest/sensingtest.ino
+++ sensingtest/sensingtest.ino
@@ -1,12 +1,24 @@
+#include <Servo.h>
+
 int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int count = 0;
+const int servoPin1 = 5;
+const int servoPin2 = 6;
 const int samples = 25; // Number of samples to average
 const int approxOrder = 3; // Order of distance approximation function (1,2,3)
 int values[samples];
+Servo servo1;
+Servo servo2; 
+int servo1Pos = 0;
+int servo2Pos = 0;
+const int servoStart = 40;
+const int servoEnd = 140;
 
 void setup() {
   Serial.begin(9600);
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
 }
 
 int average(int values[]) {
@@ -47,55 +59,43 @@ unsigned long varience(int values[], int average) {
   return normSumSquared - pow(average,2);
 }
 
-void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-  int i = count % samples;
+
+/*
+* Do a full scan of one direction
+* type: 0 = horizontal
+* type: 1 = vertical 
+*/
+void scan(int type) {
+  Servo servo;
+  int *servoPos;
+  if (!type) {
+    servo = servo1;
+    servoPos = &servo1Pos;
+  } else {
+    servo = servo2;
+    servoPos = &servo2Pos;
+  }
   int averageValue;
-  values[i] = sensorValue;
-  delay(10);
-  if (i == samples-1) {
+  for (*servoPos = servoStart; *servoPos < servoEnd; (*servoPos)++) {
+    servo.write(*servoPos);
+    for (int i = 0; i < samples; i++) {
+      values[i] = analogRead(sensorPin);
+      delay(1);
+    }
     averageValue = average(values);
-    Serial.print(average(values));
+    Serial.print(servo1Pos);
     Serial.print(",");
-    Serial.print(distance(averageValue, approxOrder));
+    Serial.print(servo2Pos);
     Serial.print(",");
-    Serial.println(varience(values, average(values)));
+    Serial.println(distance(averageValue, approxOrder));
   }
-  if (count >= 500*samples) {
-    count = 0;
-  }
-  count++;
+  //Give time to reset to start
+  servo.write(servoStart);
+  delay(300);
 }
 
-// // Sweep
-// // by BARRAGAN <http://barraganstudio.com> 
-// // This example code is in the public domain.
 
-
-// #include <Servo.h> 
- 
-// Servo myservo;  // create servo object to control a servo 
-//                 // a maximum of eight servo objects can be created 
- 
-// int pos = 0;    // variable to store the servo position 
- 
-// void setup() 
-// { 
-//   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-// } 
- 
- 
-// void loop() 
-// { 
-//   for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-//   {                                  // in steps of 1 degree 
-//     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-//     delay(15);                       // waits 15ms for the servo to reach the position 
-//   } 
-//   for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-//   {                                
-//     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-//     delay(15);                       // waits 15ms for the servo to reach the position 
-//   } 
-// } 
+void loop() {
+  scan(0);
+  scan(1);
+}
